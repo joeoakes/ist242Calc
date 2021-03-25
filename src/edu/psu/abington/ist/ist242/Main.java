cb49c1f00a686ea995fcc58b2f7b2c27ac9e49ac
joeoakes <jxo19@psu.edu>
2021-03-25 13:15:46 -0400

    Initial commit

---
+++ src/edu/psu/abington/ist/ist242/Main.java
@@ -0,0 +1,14 @@
+package edu.psu.abington.ist.ist242;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println(calc.add(10.0, 20.0));
+        System.out.println(calc.sub(10.0, 20.0));
+        System.out.println(calc.mul(10.0, 20.0));
+        System.out.println(calc.div(10.0, 20.0));
+
+    }
+}
